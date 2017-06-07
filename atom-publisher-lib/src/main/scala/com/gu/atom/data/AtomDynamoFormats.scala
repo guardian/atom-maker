837f3120840d0744b224821c8c0aceb8661cfc63
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-06-07 17:30:34 +0100

    Summon implicits in order

--- atom-publisher-lib/src/main/scala/com/gu/atom/data/AtomDynamoFormats.scala
+++ atom-publisher-lib/src/main/scala/com/gu/atom/data/AtomDynamoFormats.scala
@@ -2,41 +2,95 @@ package com.gu.atom.data
 
 import cats.syntax.either._
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
-import com.gu.contentatom.thrift.AtomData
-import com.gu.contentatom.thrift.AtomData._
+import com.gu.atom.util._
+import com.gu.contentatom.thrift.{ImageAsset, ImageAssetDimensions, Image}
 import com.gu.contentatom.thrift.atom.cta.CTAAtom
-import com.gu.contentatom.thrift.atom.explainer.ExplainerAtom
+import com.gu.contentatom.thrift.atom.explainer.{ExplainerAtom, DisplayType => EDisplayType}
 import com.gu.contentatom.thrift.atom.interactive.InteractiveAtom
-import com.gu.contentatom.thrift.atom.media.MediaAtom
-import com.gu.contentatom.thrift.atom.quiz.QuizAtom
-import com.gu.contentatom.thrift.atom.recipe.RecipeAtom
-import com.gu.contentatom.thrift.atom.review.ReviewAtom
-import com.gu.contentatom.thrift.atom.storyquestions.StoryQuestionsAtom
-import com.gu.contentatom.thrift.atom.qanda.QAndAAtom
-import com.gu.contentatom.thrift.atom.guide.GuideAtom
-import com.gu.contentatom.thrift.atom.profile.ProfileAtom
-import com.gu.contentatom.thrift.atom.timeline.TimelineAtom
+import com.gu.contentatom.thrift.atom.media.{MediaAtom, Platform, AssetType, Category, PrivacyStatus, Asset => MAsset, PlutoData => PData, Metadata}
+import com.gu.contentatom.thrift.atom.quiz.{ResultGroup, Asset => QAsset, Answer, ResultBucket, ResultBuckets, Question => QQuestion, ResultGroups, QuizContent, QuizAtom}
+import com.gu.contentatom.thrift.atom.recipe.{Tags, Time, Serves, Range, Ingredient, IngredientsList, RecipeAtom}
+import com.gu.contentatom.thrift.atom.review.{Rating, ReviewAtom}
+import com.gu.contentatom.thrift.atom.storyquestions.{Question => SQuestion, QuestionSet => SQuestionSet, StoryQuestionsAtom}
+import com.gu.contentatom.thrift.atom.qanda.{QAndAItem, QAndAAtom}
+import com.gu.contentatom.thrift.atom.guide.{GuideItem, GuideAtom}
+import com.gu.contentatom.thrift.atom.profile.{ProfileItem, ProfileAtom}
+import com.gu.contentatom.thrift.atom.timeline.{TimelineItem, TimelineAtom}
+import com.gu.contentatom.thrift.AtomData
 import com.gu.scanamo.DynamoFormat
 import com.gu.scanamo.error.{DynamoReadError, TypeCoercionError}
 import com.gu.scanamo.scrooge.ScroogeDynamoFormat._
 import com.twitter.scrooge.{ThriftStruct, ThriftStructCodec}
+import org.apache.thrift.protocol.TProtocol
 
 trait AtomDynamoFormats {
+  // summon implicits for atom parts
+  DynamoFormat[ImageAssetDimensions]
+  DynamoFormat[ImageAsset]
+  DynamoFormat[Image]
+  DynamoFormat[EDisplayType]
+  DynamoFormat[Platform]
+  DynamoFormat[AssetType]
+  DynamoFormat[Category]
+  DynamoFormat[PrivacyStatus]
+  DynamoFormat[PData]
+  DynamoFormat[Metadata]
+  DynamoFormat[ResultGroup]
+  DynamoFormat[QAsset]
+  DynamoFormat[Answer]
+  DynamoFormat[ResultBucket]
+  DynamoFormat[ResultBuckets]
+  DynamoFormat[QQuestion]
+  DynamoFormat[ResultGroups]
+  DynamoFormat[QuizContent]
+  DynamoFormat[Tags]
+  DynamoFormat[Time]
+  DynamoFormat[Serves]
+  DynamoFormat[Range]
+  DynamoFormat[Ingredient]
+  DynamoFormat[IngredientsList]
+  DynamoFormat[Rating]
+  DynamoFormat[SQuestion]
+  DynamoFormat[SQuestionSet]
+  DynamoFormat[QAndAItem]
+  DynamoFormat[GuideItem]
+  DynamoFormat[ProfileItem]
+  DynamoFormat[TimelineItem]
+
+  private def writeAtom[A <: ThriftStruct](atomData: A)(implicit f: DynamoFormat[A]): AttributeValue = f.write(atomData)
+
+  private def readAtom[A <: ThriftStruct](av: AttributeValue)(implicit f: DynamoFormat[A]): Either[DynamoReadError, A] = f.read(av)
+
+  private val allReaders = List(
+    readAtom[QuizAtom] _,
+    readAtom[MediaAtom] _,
+    readAtom[ExplainerAtom] _,
+    readAtom[CTAAtom] _,
+    readAtom[InteractiveAtom] _,
+    readAtom[ReviewAtom] _,
+    readAtom[RecipeAtom] _,
+    readAtom[StoryQuestionsAtom] _,
+    readAtom[QAndAAtom] _,
+    readAtom[GuideAtom] _,
+    readAtom[ProfileAtom] _,
+    readAtom[TimelineAtom] _
+  )
+
   implicit val dynamoFormat: DynamoFormat[AtomData] = new DynamoFormat[AtomData] {
     def write(t: AtomData) = t match {
-      case Quiz(d)            => writeAtom[QuizAtom](d)
-      case Media(d)           => writeAtom[MediaAtom](d)
-      case Explainer(d)       => writeAtom[ExplainerAtom](d)
-      case Cta(d)             => writeAtom[CTAAtom](d)
-      case Interactive(d)     => writeAtom[InteractiveAtom](d)
-      case Review(d)          => writeAtom[ReviewAtom](d)
-      case Recipe(d)          => writeAtom[RecipeAtom](d)
-      case Storyquestions(d)  => writeAtom[StoryQuestionsAtom](d)
-      case Guide(d)           => writeAtom[GuideAtom](d)
-      case Profile(d)         => writeAtom[ProfileAtom](d)
-      case Qa(d)              => writeAtom[QAndAAtom](d)
-      case Timeline(d)        => writeAtom[TimelineAtom](d)
-      case UnknownUnionField(_) => throw new RuntimeException("Unknown atom data type found.")
+      case AtomData.Quiz(d)            => writeAtom[QuizAtom](d)
+      case AtomData.Media(d)           => writeAtom[MediaAtom](d)
+      case AtomData.Explainer(d)       => writeAtom[ExplainerAtom](d)
+      case AtomData.Cta(d)             => writeAtom[CTAAtom](d)
+      case AtomData.Interactive(d)     => writeAtom[InteractiveAtom](d)
+      case AtomData.Review(d)          => writeAtom[ReviewAtom](d)
+      case AtomData.Recipe(d)          => writeAtom[RecipeAtom](d)
+      case AtomData.Storyquestions(d)  => writeAtom[StoryQuestionsAtom](d)
+      case AtomData.Guide(d)           => writeAtom[GuideAtom](d)
+      case AtomData.Profile(d)         => writeAtom[ProfileAtom](d)
+      case AtomData.Qa(d)              => writeAtom[QAndAAtom](d)
+      case AtomData.Timeline(d)        => writeAtom[TimelineAtom](d)
+      case AtomData.UnknownUnionField(_) => throw new RuntimeException("Unknown atom data type found.")
     }
 
     def read(av: AttributeValue): Either[DynamoReadError, AtomData] = {
@@ -61,23 +115,4 @@ trait AtomDynamoFormats {
       }
     }
   }
-
-  private val allReaders = List(
-    readAtom[QuizAtom] _,
-    readAtom[MediaAtom] _,
-    readAtom[ExplainerAtom] _,
-    readAtom[CTAAtom] _,
-    readAtom[InteractiveAtom] _,
-    readAtom[ReviewAtom] _,
-    readAtom[RecipeAtom] _,
-    readAtom[StoryQuestionsAtom] _,
-    readAtom[QAndAAtom] _,
-    readAtom[GuideAtom] _,
-    readAtom[ProfileAtom] _,
-    readAtom[TimelineAtom] _
-  )
-
-  private def writeAtom[A <: ThriftStruct](atomData: A)(implicit f: DynamoFormat[A]): AttributeValue = f.write(atomData)
-
-  private def readAtom[A <: ThriftStruct](av: AttributeValue)(implicit f: DynamoFormat[A]): Either[DynamoReadError, A] = f.read(av)
 }
