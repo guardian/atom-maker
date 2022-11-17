b7a57a577fc608e0bc08dc2a568f4faea216104f
Robert Kenny <kenoir@gmail.com>
2022-11-17 09:40:33 +0000

    chore(deps): bump Akka from 2.5.3 -> 2.5.16

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -4,7 +4,7 @@ object BuildVars {
   lazy val awsVersion         = "1.11.8"
   lazy val contentAtomVersion = "3.2.0"
   lazy val scroogeVersion     = "19.9.0"
-  lazy val akkaVersion        = "2.5.3"
+  lazy val akkaVersion        = "2.5.16"
   lazy val playVersion        = "2.6.0"
   lazy val mockitoVersion     = "2.0.97-beta"
 
