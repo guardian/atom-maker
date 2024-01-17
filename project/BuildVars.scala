b4d55b3dd92633ff10d8504bb7b7d4ac10fa86b8
Roberto Tyley <roberto.tyley@gmail.com>
2024-01-17 15:19:35 +0000

    Fix tests under Java 17 (Guice upgrade via Play)
    After https://github.com/guardian/atom-maker/pull/93 we found that
    unfortunately the Release workflow failed while running the tests:
    
    * The Release workflow uses Java 17 for all builds
    * The Atom Maker library uses Play with Guice for Dependency Injection
    * Guice gained Java 17 & 21 support with Guice v6:
      https://github.com/google/guice/wiki/Guice600
    * Play only updated to Guice v6 (and gained general Java 17 support)
      with Play v2.9:
      https://github.com/playframework/playframework/pull/11808
      https://github.com/playframework/playframework/commit/10ca54da9bef08551101b39b7e871cbfe894ac7b#diff-3dc52110c1c1c453c2e9740ac58fe7e90d53121875a034ef3109c34ab030c29e

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -4,6 +4,6 @@ object BuildVars {
   lazy val awsVersion         = "1.11.8"
   lazy val contentAtomVersion = "4.0.0"
   lazy val scroogeVersion     = "22.1.0"
-  lazy val playVersion        = "2.8.8"
-  lazy val mockitoVersion     = "4.8.0"
+  lazy val playVersion        = "2.9.1"
+  lazy val mockitoVersion     = "4.11.0"
 }
