43b21b5c988f46ee56361fcdcd112c5363d6aac8
fweddi <freddiepreece@outlook.com>
2025-09-03 16:57:19 +0100

    Update to latest atom model

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildVars {
   lazy val awsVersion         = "1.12.680"
-  lazy val contentAtomVersion = "4.0.1"
+  lazy val contentAtomVersion = "6.1.0"
   lazy val scroogeVersion     = "22.1.0"
   lazy val playVersion        = "3.0.2"
   lazy val mockitoVersion     = "4.11.0"
