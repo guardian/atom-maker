d5f902bfdd7a41cab7c1bc93a09667183770fa12
Pete F <pete.faulconbridge@theguardian.com>
2025-10-31 09:22:23 +0000

    Test preview release of content-atom-model

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildVars {
   lazy val awsVersion         = "1.12.680"
-  lazy val contentAtomVersion = "7.0.7"
+  lazy val contentAtomVersion = "8.0.0-PREVIEW.pficonik-data.2025-10-31T0908.09341485"
   lazy val scroogeVersion     = "22.1.0"
   lazy val playVersion        = "3.0.2"
   lazy val mockitoVersion     = "4.11.0"
