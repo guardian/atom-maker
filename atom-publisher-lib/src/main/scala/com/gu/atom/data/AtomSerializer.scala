8df78427b6a6f839df599412de924aa7b19b4a32
lindseydew <lindsey.dew@guardian.co.uk>
2025-12-04 15:18:04 +0000

    Import rejig

--- atom-publisher-lib/src/main/scala/com/gu/atom/data/AtomSerializer.scala
+++ atom-publisher-lib/src/main/scala/com/gu/atom/data/AtomSerializer.scala
@@ -3,7 +3,8 @@ package com.gu.atom.data
 import com.gu.contentatom.thrift.Atom
 import io.circe.Json
 import io.circe.syntax._
-import com.gu.fezziwig.CirceScroogeMacros.encodeThriftStruct
+import com.gu.fezziwig.CirceScroogeMacros.{encodeThriftStruct, encodeThriftUnion}
+import com.gu.atom.util.JsonSupport.{backwardsCompatibleAtomDecoder, thriftEnumEncoder}
 object AtomSerializer {
 
   def toJson(newAtom: Atom): Json = newAtom.asJson
