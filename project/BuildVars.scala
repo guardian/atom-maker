eaca40a81670691455e6873e0fc8d5b3f7513f32
Anna Beddow <anna.beddow@gmail.com>
2026-04-09 16:12:42 +0100

    Bump contentAtomVersion to PREVIEW version "10.0.0-PREVIEW.abhas-audio-media-atom.2026-04-02T1532.559b1cde"

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildVars {
   lazy val awsVersion         = "1.12.680"
-  lazy val contentAtomVersion = "9.0.0"
+  lazy val contentAtomVersion = "10.0.0-PREVIEW.abhas-audio-media-atom.2026-04-02T1532.559b1cde"
   lazy val scroogeVersion     = "22.1.0"
   lazy val playVersion        = "3.0.10"
   lazy val mockitoVersion     = "4.11.0"
