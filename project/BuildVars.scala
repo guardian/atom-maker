f5fe6dd879150f1c8443cde296265e43f36a7f44
Anna Beddow <anna.beddow@gmail.com>
2026-04-16 09:44:46 +0100

    Update BuildVars.scala

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildVars {
   lazy val awsVersion         = "1.12.680"
-  lazy val contentAtomVersion = "10.0.0-PREVIEW.abhas-audio-media-atom.2026-04-13T1234.ab69ccf7"
+  lazy val contentAtomVersion = "10.0.0-PREVIEW.abhas-audio-media-atom.2026-04-16T0842.e5896400"
   lazy val scroogeVersion     = "22.1.0"
   lazy val playVersion        = "3.0.10"
   lazy val mockitoVersion     = "4.11.0"
