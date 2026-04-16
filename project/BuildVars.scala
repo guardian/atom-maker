d183be56f03b1158c981f618d046a3ed175f8e30
Sam Hession <sam.hession@guardian.co.uk>
2026-04-16 11:12:50 +0100

    Uses the preview release of the content atom asset with a duration attribute

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildVars {
   lazy val awsVersion         = "1.12.680"
-  lazy val contentAtomVersion = "9.0.0"
+  lazy val contentAtomVersion = "10.0.0-PREVIEW.shadd-duration-to-media-asset-type.2026-04-16T0827.ca8a1298"
   lazy val scroogeVersion     = "22.1.0"
   lazy val playVersion        = "3.0.10"
   lazy val mockitoVersion     = "4.11.0"
