35727cd26e810e6a6b1803d2eae03224098da532
Georges Lebreton <georges.lebreton@guardian.co.uk>
2026-08-04 10:42:28 +0100

    add multimedia slideshow

--- project/BuildVars.scala
+++ project/BuildVars.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object BuildVars {
   lazy val awsVersion         = "1.12.680"
-  lazy val contentAtomVersion = "12.0.1"
+  lazy val contentAtomVersion = "15.1.0-PREVIEW.glspike-multimedia-atom.2026-09-01T2238.c8b18f89"
   lazy val scroogeVersion     = "22.1.0"
   lazy val playVersion        = "3.0.10"
   lazy val mockitoVersion     = "4.11.0"
